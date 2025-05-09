7a9e1872ff7865c44d28bd45e287b479ccf940fc
Pannawich Thamart <MadManJJ4949@gmail.com>
2025-05-09 23:33:37 +0700

    Add a simple endpoint

--- main.go
+++ main.go
@@ -1,9 +1,20 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/MadManJJ/go-todo-api/db"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	db.InitDB()
+
+	router := gin.Default()
+
+	router.GET("/todos", func(c *gin.Context) {
+		c.IndentedJSON(http.StatusOK, "HI")
+	})
+
+	router.Run("localhost:8080")
 }
